7d92149e4d3837032ae35b31d4e94ddb5caecb8e
Rainaldo Crosbourne <rcrosbourne@gmail.com>
2014-01-19 19:29:46 -0500

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrApi::Application.config.secret_key_base = '0a4394bcdaf2a6bfbdd6bae41169af528cd4676a2f7cff05b8ea1d25094f4171641ad7e036d685215afdbabd508fde7f0aaff62fe09225a4df838dd8b93996e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UrApi::Application.config.session_store :cookie_store, key: '_ur_api_session'
